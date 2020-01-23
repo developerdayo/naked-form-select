a79364cf87c58e31b51c1d111011175602d48c16
Sarah Ferguson <sarah.an.ferguson@gmail.com>
2020-01-23 12:27:58 -0500

    update demo file to include demonstration of context option

--- demo/demo.js
+++ demo/demo.js
@@ -1,8 +1,14 @@
 function ready() {
-  nakedFormSelect('select:not([multiple])');
-  nakedFormSelect('select[multiple]', {
+  nakedFormSelect('.example-1');
+  nakedFormSelect('.example-3', {
     keywordSearch: {
       on: true,
+    }
+  });
+  nakedFormSelect('.example-4', {
+    context: {
+      value: '.section-with-context',
+      queryDocument: true
     }
   });
   function postResults() {
@@ -12,6 +18,7 @@ function ready() {
     const select1 = document.querySelector('[name="select1"]');
     const select2 = document.querySelector('[name="select2"]');
     const select3 = document.querySelector('[name="select3"]');
+    const select4 = document.querySelector('[name="select4"]');
 
     $submitBtn.addEventListener('click', (event) => {
       $formResults.innerHTML = '';
@@ -22,6 +29,7 @@ function ready() {
       Array.from(select1.selectedOptions).forEach((option) => { results.push(option.value) })
       Array.from(select2.selectedOptions).forEach((option) => { results.push(option.value) })
       Array.from(select3.selectedOptions).forEach((option) => { results.push(option.value) })
+      Array.from(select4.selectedOptions).forEach((option) => { results.push(option.value) })
 
       results.forEach((selectedOptionsSet) => {
         let $container = document.createElement('div');
